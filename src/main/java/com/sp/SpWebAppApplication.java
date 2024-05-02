37a435da79b035990714243237903b94239dd79c
daddyornot <damienmailhebiau@gmail.com>
2024-05-02 14:27:04 +0200

    init

---
+++ src/main/java/com/sp/SpWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWebAppApplication.class, args);
+	}
+
+}
