8c58665fecd3c353c002c0a199a50cf4d42650df
Maxime DUBOIS <maxime-m.dubois@edf.fr>
2024-05-21 16:17:13 +0200

    feat: transformation en API

--- atelier2/src/main/java/com/sp/controller/AuthController.java
+++ atelier2/src/main/java/com/sp/controller/AuthController.java
@@ -1,15 +1,14 @@
 package com.sp.controller;
 
 import com.sp.model.dto.AuthDTO;
-import com.sp.model.dto.UserDTO;
 import com.sp.service.AuthService;
-import com.sp.utils.CookieUtil;
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,59 +21,33 @@ public class AuthController {
     @Autowired
     AuthService authService;
 
-    private String goToLogin(Model model) {
-
-        return "redirect:/login";
-    }
-
-    private String goToSuccess() {
-        return "redirect:/addCard";
-    }
-
-    @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
-    public String login(HttpServletRequest request, Model model) {
-        String sessionToken = CookieUtil.getCookieValue(request, "Token");
-
-        if (sessionToken == null) {
-            AuthDTO authDTO = new AuthDTO();
-            model.addAttribute("authDTO", authDTO);
-            return "login";
-        }
-        UserDTO userDTO = authService.getLoggedUser(request);
-        if (userDTO != null) {
-            System.out.println(userDTO.getSurname() + " is logged in");
-            return goToSuccess();
-        }
-        return goToLogin(model);
-    }
-
     @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
-    public String auth(Model model, AuthDTO authDTO, HttpServletResponse response) {
+    public ResponseEntity<String> auth(Model model, @RequestBody AuthDTO authDTO, HttpServletResponse response) {
         Cookie cookie = authService.authenticate(authDTO);
         if (cookie != null) {
             response.addCookie(cookie);
-            return goToSuccess();
+            return ResponseEntity.ok("Login successful");
         }
         //login fail
-        return goToLogin(model);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
     }
 
     @RequestMapping(value = {"/logout"}, method = RequestMethod.GET)
-    public String logout(Model model, HttpServletResponse response) {
+    public ResponseEntity<String> logout(Model model, HttpServletResponse response) {
         Cookie cookie = authService.logout();
         // Ajouter le cookie à la réponse
         response.addCookie(cookie);
-        return goToLogin(model);
+        return ResponseEntity.ok("Logout successful");
     }
 
     @RequestMapping(value = {"/register"}, method = RequestMethod.POST)
-    public String register(Model model, AuthDTO authDTO, HttpServletResponse response) {
+    public ResponseEntity<String> register(Model model, @RequestBody AuthDTO authDTO, HttpServletResponse response) {
         Cookie cookie = authService.register(authDTO);
         if (cookie == null) {
-            return goToLogin(model);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Registration successful");
         }
         response.addCookie(cookie);
-        return goToSuccess();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Registration failed");
     }
 
 
