4fe75927d6df85502f2dd25c6d9ed944f2336f7c
Matthieu LAPETITTE <matthieu@lapetitte.fr>
2024-05-27 15:25:33 +0200

    Add Lombok dependency and update UserAdd class
    A dependency for the Lombok library has been added to the project. The 'UserAdd' class was updated to implement the 'JavaDelegate' interface, extending its functionality with a rollback mechanism for address update tasks. Also, Lombok annotations were added for cleaner and simpler code.

--- atelier3/orchestrator/src/main/java/com/sp/registrer/UserAdd.java
+++ atelier3/orchestrator/src/main/java/com/sp/registrer/UserAdd.java
@@ -1,4 +1,34 @@
 package com.sp.registrer;
 
-public class UserAdd {
+import jakarta.inject.Named;
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.*;
+
+@Component
+@Named
+@RequiredArgsConstructor
+@Log4j2
+public class UserAdd implements JavaDelegate {
+    private final WebClient.Builder webClientBuilder;
+    @Value("${endpoint.addresses}")
+    protected String addressesEndpoint;
+    private final AddressVariableRetrieverForRollBack addressAdapterParamToRollBack;
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        try {
+            AddressDTO savedAddress =
+                    addressAdapterParamToRollBack.getAddressDTOtoRollback(delegateExecution);
+            webClientBuilder.build().delete()
+                    .uri(addressesEndpoint + "/" + savedAddress.getCode())
+                    .retrieve()
+                    .bodyToMono(Void.class).block();
+        } catch (WebClientResponseException ex) {
+            log.error("An error has occurred while rolling back address update task : {}", ex.getMessage());
+            throw new ProcessException("An error has occurred while rolling back address update task: " +
+                    ex.getMessage());
+        }
+    }
 }
