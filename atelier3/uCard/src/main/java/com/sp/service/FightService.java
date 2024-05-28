ac43492daede6fdee3e56376964e87fb19847dd2
Maxime DUBOIS <maxime-m.dubois@edf.fr>
2024-05-28 17:27:39 +0200

    feat: regain fonction

--- atelier3/uCard/src/main/java/com/sp/service/FightService.java
+++ atelier3/uCard/src/main/java/com/sp/service/FightService.java
@@ -6,4 +6,6 @@ public interface FightService {
 
     public Long fight(FightDTO fightDTO);
 
+    public void regainEnergyOnAllCard();
+
 }
--- atelier3/uCard/src/main/java/com/sp/service/PeriodicEnergyRegain.java
+++ atelier3/uCard/src/main/java/com/sp/service/PeriodicEnergyRegain.java
@@ -1,9 +1,18 @@
 package com.sp.service;
 
+import com.sp.service.FightService;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class PeriodicEnergyRegain {
+
+    private static FightService fightService = null;
+
+    public PeriodicEnergyRegain(FightService fightService) {
+        this.fightService = fightService;
+    }
 
     public static void main(String[] args) {
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
@@ -12,10 +21,11 @@ public class PeriodicEnergyRegain {
             @Override
             public void run() {
                 System.out.println("Regain energy");
+                fightService.regainEnergyOnAllCard();
             }
         };
 
-        scheduler.scheduleAtFixedRate(task, 0, 10, java.util.concurrent.TimeUnit.SECONDS);
+        scheduler.scheduleAtFixedRate(task, 0, 10, TimeUnit.SECONDS);
 
         try {
             Thread.sleep(Long.MAX_VALUE);
