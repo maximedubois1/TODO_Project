f5402d1941d26dc95fd70c364346622929594d31
Maxime DUBOIS <maxime-m.dubois@edf.fr>
2024-05-28 10:28:53 +0200

    fix: name app

--- atelier3/uAuth/src/main/java/com/sp/AuthMicroService.java
+++ atelier3/uAuth/src/main/java/com/sp/AuthMicroService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpAppHero {
+public class AuthMicroService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpAppHero.class, args);
+		SpringApplication.run(AuthMicroService.class, args);
 	}
 
 }
