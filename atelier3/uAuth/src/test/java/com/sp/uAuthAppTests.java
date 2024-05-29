f64ea13512ce56ea0e61e8134576909daa3ea4a0
Maxime DUBOIS <maxime-m.dubois@edf.fr>
2024-05-29 14:33:52 +0200

    rebase2

--- atelier3/uAuth/src/test/java/com/sp/uAuthAppTests.java
+++ atelier3/uAuth/src/test/java/com/sp/uAuthAppTests.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+<<<<<<<< HEAD:atelier3/uAuth/src/test/java/com/sp/uAuthAppTests.java
 class uAuthAppTests {
+========
+class AuthMicroServiceTests {
+>>>>>>>> f5402d1 (fix: name app):atelier3/uAuth/src/test/java/com/sp/AuthMicroServiceTests.java
 
 	@Test
 	void contextLoads() {
