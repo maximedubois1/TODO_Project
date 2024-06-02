dc7f2d92bd58b0a8f9f6fe23b0d6636fdff94bd4
daddyornot <damienmailhebiau@gmail.com>
2024-06-02 14:43:29 +0200

    feat: some fixes to work with everything (orchestrator etc..), NOT TOTALLY WORKING

--- atelier3/uAuth/src/main/java/com/sp/utils/security/JwtAuthFilter.java
+++ atelier3/uAuth/src/main/java/com/sp/utils/security/JwtAuthFilter.java
@@ -27,29 +27,6 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         this.userDetailsServiceImpl = userDetailsServiceImpl;
     }
 
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//
-//        String authHeader = request.getHeader("Authorization");
-//        String token = null;
-//        String username = null;
-//        if(authHeader != null && authHeader.startsWith("Bearer ")){
-//            token = authHeader.substring(7);
-//            username = jwtService.extractUsername(token);
-//        }
-//
-//        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
-//            UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
-//            if(jwtService.validateToken(token, userDetails)){
-//                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-//                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//            }
-//        }
-//
-//        filterChain.doFilter(request, response);
-//    }
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
